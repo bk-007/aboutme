484a3af648f28a4620a723ecefff239b00ad4000
Bryce <51759831+bk-007@users.noreply.github.com>
2023-07-01 10:27:52 -0700

    added js for loader

---
+++ myscripts.js
@@ -0,0 +1,35 @@
+document.addEventListener("DOMContentLoaded", function() {
+  // Select the loader and animation elements
+  var loader = document.querySelector(".loader");
+  var animation = document.querySelector(".animation");
+  var buttons = document.querySelector(".buttons");
+
+  // Show the loader
+  function showLoader() {
+    loader.style.display = "flex";
+    animation.style.display = "none";
+    buttons.style.display = "none";
+  }
+
+  // Hide the loader and show the animation and buttons
+  function hideLoader() {
+    loader.style.display = "none";
+    animation.style.display = "block";
+    buttons.style.display = "block";
+  }
+
+  // Simulate a loading delay (you can replace this with your own loading logic)
+  function simulateLoading() {
+    // Show the loader immediately
+    showLoader();
+
+    // Simulate loading delay (e.g., 3 seconds)
+    setTimeout(function() {
+      // Hide the loader and show the animation and buttons after the delay
+      hideLoader();
+    }, 3000); // Adjust the delay time as needed
+  }
+
+  // Call the simulateLoading function to start the loading process
+  simulateLoading();
+});
