edeaf20a23dc8be6287d6ba7ae682f6824550926
Bryce <51759831+bk-007@users.noreply.github.com>
2023-07-01 11:18:46 -0700

    lots of changes

--- myscripts.js
+++ myscripts.js
@@ -1,35 +1,71 @@
-document.addEventListener("DOMContentLoaded", function() {
-  // Select the loader and animation elements
-  var loader = document.querySelector(".loader");
-  var animation = document.querySelector(".animation");
-  var buttons = document.querySelector(".buttons");
+// loader and background switcher based on screen size
+    document.addEventListener("DOMContentLoaded", function() {
+      // Select the loader and animation elements
+      var loader = document.querySelector(".loader");
+      var animation = document.querySelector(".animation");
+      var buttons = document.querySelector(".buttons");
 
-  // Show the loader
-  function showLoader() {
-    loader.style.display = "flex";
-    animation.style.display = "none";
-    buttons.style.display = "none";
-  }
+      // Show the loader
+      function showLoader() {
+        loader.style.display = "flex";
+        animation.style.display = "none";
+        buttons.style.display = "none";
+      }
 
-  // Hide the loader and show the animation and buttons
-  function hideLoader() {
-    loader.style.display = "none";
-    animation.style.display = "block";
-    buttons.style.display = "block";
-  }
+      // Hide the loader and show the animation and buttons
+      function hideLoader() {
+        loader.style.display = "none";
+        animation.style.display = "block";
+        buttons.style.display = "block";
+      }
 
-  // Simulate a loading delay (you can replace this with your own loading logic)
-  function simulateLoading() {
-    // Show the loader immediately
-    showLoader();
+      // Set the background animation source based on the device display size
+      function setAnimationSource() {
+        var backgroundAnimation = document.getElementById("background-animation");
+        var animationSource = window.innerWidth <= 767 ? "json/mobile-background.json" : "json/desktop-background.json";
+        backgroundAnimation.setAttribute("src", animationSource);
+      }
 
-    // Simulate loading delay (e.g., 3 seconds)
-    setTimeout(function() {
-      // Hide the loader and show the animation and buttons after the delay
-      hideLoader();
-    }, 3000); // Adjust the delay time as needed
-  }
+      // Simulate a loading delay (you can replace this with your own loading logic)
+      function simulateLoading() {
+        // Show the loader immediately
+        showLoader();
 
-  // Call the simulateLoading function to start the loading process
-  simulateLoading();
-});
+        // Simulate loading delay (e.g., 3 seconds)
+        setTimeout(function() {
+          // Hide the loader and show the animation and buttons after the delay
+          hideLoader();
+        }, 3000); // Adjust the delay time as needed
+      }
+
+      // Call the setAnimationSource function to set the appropriate background animation
+      setAnimationSource();
+
+      // Call the simulateLoading function to start the loading process
+      simulateLoading();
+    });
+
+
+// lottie buttons clickable
+    document.addEventListener("DOMContentLoaded", function() {
+      // Set up Lottie animations for each button
+      var lottieButtons = document.querySelectorAll(".lottie-button");
+
+      lottieButtons.forEach(function(button) {
+        var link = button.getAttribute("data-link");
+        var animationId = button.querySelector("div").id;
+
+        var animation = bodymovin.loadAnimation({
+          container: document.getElementById(animationId),
+          renderer: "svg",
+          loop: true,
+          autoplay: true,
+          path: "path/to/lottie/" + animationId + ".json" // Replace with the path to your Lottie animation JSON files
+        });
+
+        // Add click event listener to navigate to the specified link on button click
+        button.addEventListener("click", function() {
+          window.location.href = link;
+        });
+      });
+    });
