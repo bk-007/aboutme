84c153c4b458fd002e576a3eebaad15a8bf2e9e5
bk-007 <bry2163447@maricopa.edu>
2023-07-01 14:49:55 -0700

    remove background

--- myscripts.js
+++ myscripts.js
@@ -38,57 +38,10 @@ document.addEventListener("DOMContentLoaded", function() {
 
   lottieButtons.forEach(function(button) {
     var link = button.getAttribute("data-link");
-    var animationId = button.querySelector("div").id;
-
-    var animation = bodymovin.loadAnimation({
-      container: document.getElementById(animationId),
-      renderer: "svg",
-      loop: true,
-      autoplay: true,
-      path: "json/" + animationId + ".json"
-    });
 
     // Add click event listener to navigate to the specified link on button click
     button.addEventListener("click", function() {
       window.location.href = link;
     });
   });
-
-  // Function to create a new <lottie-player> element with the specified src
-  function createLottiePlayer(src) {
-    var container = document.getElementById("animation-container");
-
-    // Remove any existing <lottie-player> elements
-    while (container.firstChild) {
-      container.removeChild(container.firstChild);
-    }
-
-    // Create a new <lottie-player> element
-    var lottiePlayer = document.createElement("lottie-player");
-    lottiePlayer.setAttribute("id", "background-animation");
-    lottiePlayer.setAttribute("background", "transparent");
-    lottiePlayer.setAttribute("speed", "1");
-    lottiePlayer.setAttribute("style", "width: 100%; height: 100%;");
-    lottiePlayer.setAttribute("loop", "");
-    lottiePlayer.setAttribute("autoplay", "");
-    lottiePlayer.setAttribute("src", src);
-
-    // Append the new <lottie-player> element to the container
-    container.appendChild(lottiePlayer);
-  }
-
-  // Function to update the background animation based on the screen size
-  function updateBackgroundAnimation() {
-    if (window.innerWidth <= 767) {
-      createLottiePlayer("json/mobile-background.json");
-    } else {
-      createLottiePlayer("json/desktop-background.json");
-    }
-  }
-
-  // Call the function initially to set the background animation
-  updateBackgroundAnimation();
-
-  // Listen for window resize events and update the background animation
-  window.addEventListener("resize", updateBackgroundAnimation);
 });
